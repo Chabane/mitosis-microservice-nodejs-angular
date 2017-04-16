43c0e37418b85096c8003c6e7af276078a8f5775
Chabane <chabane.refes@gmail.com>
2017-04-16 16:42:01 +0200

    upgrade to angular 4

--- server/app.js
+++ server/app.js
@@ -4,34 +4,34 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan'); // logger
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname + '/../dist'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(morgan('dev'));
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://mongo:27017/mitosis');
 const db = mongoose.connection;
+mongoose.connect('mongodb://mongo:27017/mitosis');
 mongoose.Promise = global.Promise;
 
-
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log('Connected to MongoDB');
 
   // all other routes are handled by Angular
-  app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname,'/../dist/index.html'));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../dist/index.html'));
   });
 
-  app.listen(app.get('port'), function() {
-    console.log('Mitosis NodeJS App listening on port '+app.get('port'));
+  app.listen(app.get('port'), function () {
+    console.log('Mitosis NodeJS App listening on port ' + app.get('port'));
   });
 });
 
