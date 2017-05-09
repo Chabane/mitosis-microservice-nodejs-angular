2513cba0574d5cef2dc1b0968e8f071a2fd97cba
Chabane <chabane.refes@gmail.com>
2017-05-09 23:29:17 +0200

    test with mip

--- server/index.js
+++ server/index.js
@@ -29,3 +29,29 @@ require('./db/user');
 require('./kafka/consumer');
 
 module.exports = app;
+
+var wowweemip = require("wowweemip");
+var mipFinder = new wowweemip.Finder();
+
+mipFinder.scan(function(err, robots) {
+  if (err != null) {
+    console.log(err);
+    return;
+  }
+
+  //connect to first mip
+  var selectedMip = robots[0];
+  mipFinder.connect(selectedMip, function(err) {
+    if (err != null) {
+      console.log(err);
+      return;
+    }
+
+    console.log("connected");
+
+    //move toward
+    selectedMip.driveDistanceByCm(20, 0, function(err) {
+      console.log("moving toward");
+    });
+  });
+});
