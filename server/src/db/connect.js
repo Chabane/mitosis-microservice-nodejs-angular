5eeca0fa09cbae39d3e9acbad92a625768114766
Chabane <chabane.refes@gmail.com>
2017-07-25 17:07:19 +0200

    bug(db/connect.js): change ip address to the container name

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 
 const db = mongoose.connection;
-mongoose.connect('mongodb://192.168.1.108:27017/mitosis');
+mongoose.connect('mongodb://mongo:27017/mitosis');
 mongoose.Promise = global.Promise;
 
 db.on('error', console.error.bind(console, 'connection error:'));
