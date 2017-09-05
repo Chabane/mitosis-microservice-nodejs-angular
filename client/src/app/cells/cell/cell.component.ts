f2322cfecb991db248773e4b1813d66115883e66
Chabane <chabane.refes@gmail.com>
2017-09-05 22:07:58 +0200

    build:remove unused component

--- client/src/app/cells/cell/cell.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mi-cell',
-  templateUrl: './cell.component.html',
-  styleUrls: ['./cell.component.css']
-})
-export class CellComponent {
-}
